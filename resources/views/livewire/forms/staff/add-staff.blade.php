e712e3ac7291f2ceada8ac03901a87e23cba85ca
StartappzDev <startappzlimited01@gmail.com>
2024-06-24 15:56:34 +0300

    Added staff and customer create functionality.

--- resources/views/livewire/forms/staff/add-staff.blade.php
+++ resources/views/livewire/forms/staff/add-staff.blade.php
@@ -1,12 +1,61 @@
 <?php
 
+use App\Models\Staff;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Volt\Component;
 
 new class extends Component {
+
+    use LivewireAlert;
+
+
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $phone_number;
+
+    public function rules()
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|unique:staff',
+            'phone_number' => 'required|unique:staff',
+        ];
+    }
+
+
+    public function createStaff()
+    {
+
+        $this->validate();
+
+        DB::begintransaction();
+        try {
+
+            Staff::create([
+                'first_name' => $this->first_name,
+                'last_name' => $this->last_name,
+                'email' => $this->email,
+                'phone_number' => $this->phone_number
+            ]);
+
+            DB::commit();
+            $this->alert('success', 'Staff Created Successfully');
+
+        } catch (Exception $exception) {
+
+            DB::rollBack();
+            $this->alert('error', $exception->getMessage());
+        }
+
+
+    }
 
 
 }; ?>
@@ -20,35 +69,44 @@ new class extends Component {
         </div>
         <div class="card-body p-4">
             <div class="row">
-                <form wire:submit="addProduct" class="mt-6 space-y-6">
+                <form wire:submit="createStaff" class="mt-6 space-y-6">
                     <div class="row">
                         <div class="mb-4 col-lg-6">
-                            <label for="name" class="form-label">First Name</label>
-                            <input wire:model="name" id="name" class="form-control" type="text"
-                                   required autofocus autocomplete="name">
-                            <x-input-error class="mt-2" :messages="$errors->get('name')"/>
+                            <label for="first_name" class="form-label">First Name</label>
+                            <input wire:model="first_name" id="first_name" class="form-control" type="text"
+                                    autofocus autocomplete="first_name">
+                            @error('first_name')
+                            <p class="text-danger text-xs pt-1"> {{ $message }} </p>
+                            @enderror
 
                         </div>
                         <div class="mb-4 col-lg-6">
-                            <label for="name" class="form-label">Last Name</label>
-                            <input wire:model="name" id="name" class="form-control" type="text"
-                                   required autofocus autocomplete="name">
+                            <label for="last_name" class="form-label">Last Name</label>
+                            <input wire:model="last_name" id="last_name" class="form-control" type="text"
+                                    autofocus autocomplete="last_name">
+                            @error('last_name')
+                            <p class="text-danger text-xs pt-1"> {{ $message }} </p>
+                            @enderror
 
                         </div>
                     </div>
                     <div class="row">
                         <div class="mb-4 col-lg-6">
-                            <label for="name" class="form-label">Email</label>
-                            <input wire:model="name" id="name" class="form-control" type="text"
-                                   required autofocus autocomplete="name">
-                            <x-input-error class="mt-2" :messages="$errors->get('name')"/>
+                            <label for="email" class="form-label">Email</label>
+                            <input wire:model="email" id="email" class="form-control" type="text"
+                                    autofocus autocomplete="email">
+                            @error('email')
+                            <p class="text-danger text-xs pt-1"> {{ $message }} </p>
+                            @enderror
 
                         </div>
                         <div class="mb-4 col-lg-6">
-                            <label for="name" class="form-label">Phone Number</label>
-                            <input wire:model="name" id="name" class="form-control" type="text"
-                                   required autofocus autocomplete="name">
-
+                            <label for="phone_number" class="form-label">Phone Number</label>
+                            <input wire:model="phone_number" id="phone_number" class="form-control" type="text"
+                                    autofocus autocomplete="phone_number">
+                            @error('phone_number')
+                            <p class="text-danger text-xs pt-1"> {{ $message }} </p>
+                            @enderror
                         </div>
                     </div>
                     <button type="submit" class="btn btn-outline-secondary waves-effect">Add Staff</button>
