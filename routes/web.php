4546bdc5f001edae948d176f5a2516b153d7ec21
KEVINAMAYI <kevinamayi20@gmail.com>
2024-08-11 16:09:11 +0300

    symbolic link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/auth.php';
@@ -8,10 +9,7 @@ require __DIR__ . '/front-end/web.php';
 require __DIR__ . '/dashboard/web.php';
 
 //create symbolic link
-Route::get('/sym', function () {
-
-    symlink('/home/gsminter/public_html/storage/app/public', '/home/gsminter/public_html/public/storage');
-    dd('Symlink created successfully.');
-
-});
+Route::get('/symbolic-link', function () {
+    Artisan::call('symbolic:link');
+})->name('symbolic-link');
 
