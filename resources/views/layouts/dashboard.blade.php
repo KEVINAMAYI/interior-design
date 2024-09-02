ba44e9633dbad8ba6f1b37057196da5bea8c86f0
KEVINAMAYI <kevinamayi20@gmail.com>
2024-09-02 21:31:12 +0300

    Update CallBack Status Functionality

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -34,7 +34,7 @@
 <div class="rightbar-overlay"></div>
 
 <script src="dashboard/libs/jquery/jquery.min.js"></script>
-<script data-navigate-once src="dashboard/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="dashboard/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="dashboard/libs/metismenu/metisMenu.min.js"></script>
 <script src="dashboard/libs/simplebar/simplebar.min.js"></script>
 <script src="dashboard/libs/node-waves/waves.min.js"></script>
@@ -111,7 +111,6 @@
 
         // Initialize DataTables on first load
         initializeAllDataTables();
-
 
     });
 </script>
